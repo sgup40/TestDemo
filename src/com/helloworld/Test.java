cf37b9a13ec2287e65d5e1a996ddd968773e4b0d
sumit <sgupta40@sapient.com>
2015-02-04 15:24:18 +0530

    commit the message

--- src/com/helloworld/Test.java
+++ src/com/helloworld/Test.java
@@ -7,8 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Sumit this sidedsdsdsdsds
-		//sasadsaasdsadsa
+		
 		System.out.println("Hello world Release branch");
 		//Added one more comment
 
