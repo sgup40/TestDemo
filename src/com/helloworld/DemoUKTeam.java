cb14e2b1830bd51b3dfac7d5e172df4e7fc2c991
sumit <sgupta40@sapient.com>
2015-02-16 18:44:55 +0530

    Changed the value of UK DMEO TEAm

--- src/com/helloworld/DemoUKTeam.java
+++ src/com/helloworld/DemoUKTeam.java
@@ -6,10 +6,7 @@ public class DemoUKTeam {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Demo Line to be addedsadas
-		System.out.println("Hi All Generating the branch");
-		System.out.println("adsfdsfs");
+		// Deleted
 	}
 
 }
